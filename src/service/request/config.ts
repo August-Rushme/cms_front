8f0cd1dd401551544eaed38f139ae734950716be
“Augsut-Rushme” <864011713@qq.com>
2021-10-19 10:26:28 +0800

    fix: fix url

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -2,7 +2,7 @@
  * @Author: August
  * @Date: 2021-09-25 11:46:41
  * @LastEditors: August
- * @LastEditTime: 2021-10-19 01:25:43
+ * @LastEditTime: 2021-10-19 10:25:45
  * @FilePath: \vue_shop_servere:\rookie-cms\src\service\request\config.ts
  */
 let BASE_URL = ''
@@ -11,6 +11,6 @@ if (process.env.NODE_ENV === 'development') {
   BASE_URL = 'http://127.0.0.1:8081'
   // BASE_URL = 'http://152.136.185.210:5000'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://8.129.76.241:8081'
+  BASE_URL = 'http://localhost:8081'
 }
 export { BASE_URL, TIME_OUT }
